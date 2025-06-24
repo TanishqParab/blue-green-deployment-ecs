c90571496857805e404cde844f822f7d0b875534
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-06-24 19:49:13 +0530

    Delete blue-green-deployment directory

--- blue-green-deployment/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = [for subnet in aws_subnet.public_subnets : subnet.id]
-}
