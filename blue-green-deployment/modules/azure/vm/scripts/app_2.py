95b4666831c8a02a6f7f9f310adc13846b9e3c34
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-07-09 19:09:25 +0530

    Update app_2.py

--- blue-green-deployment/modules/azure/vm/scripts/app_2.py
+++ blue-green-deployment/modules/azure/vm/scripts/app_2.py
@@ -10,7 +10,7 @@ blog_posts = [
     {
         "id": "1",
         "title": "Welcome to App 2 Blue-Green Deployment Blog",
-        "content": "This is a demo #26 of App 2 blog application running with blue-green deployment on Azure VM.",
+        "content": "This is a demo #28 of App 2 blog application running with blue-green deployment on Azure VM.",
         "author": "Admin",
         "date": "2023-06-15"
     },
